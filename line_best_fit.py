425fccd51ef37f700c1ca3a18db726e10ed11ade
Eli Pandolfo <epandolf@ucsc.edu>
2017-08-30 11:25:49 -0700

    fixed issue when xdata nd ydata were lists instead of ndarrays

--- line_best_fit.py
+++ line_best_fit.py
@@ -5,12 +5,18 @@ import numpy as np
 
 # every x value should have a corresponding y value
 def best_fit(xdata, ydata):
-	# turn lists into nparrays if they are not already
+
+	# turn lists into ndarrays if they are not already
+	if not isinstance(xdata, np.ndarray):
+		xdata = np.array(xdata)
+	if not isinstance(ydata, np.ndarray):
+		ydata = np.array(ydata)
+
 	if len(xdata) != len(ydata):
-		print(len(xdata), " x data points and", len(ydata), 
-		      "y data points; please ensure your xdata and ydata 
+		print(len(xdata), " x data points and", len(ydata),
+		      "y data points; please ensure your xdata and ydata \
 		      correspond.\n Your results will be skewed.")
-	m = ((np.mean(xdata) * np.mean(ydata) - np.mean(xdata * ydata)) 
+	m = ((np.mean(xdata) * np.mean(ydata) - np.mean(xdata * ydata))
 	     / (np.mean(xdata)**2 - np.mean(xdata**2)))
 	b = np.mean(ydata) - m * np.mean(xdata)
 	return m, b
