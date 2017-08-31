041ab5d82e529c4cc34cfe843162c0ee4d446838
Eli Pandolfo <epandolf@ucsc.edu>
2017-08-31 14:53:02 -0700

    pep 8

--- euclidian_dist.py
+++ euclidian_dist.py
@@ -2,8 +2,9 @@
 # p1 and p2 can be lists
 def euclidian_dist1(p1, p2):
 	if len(p1) != len(p2):
-		print('p1 has', len(p1), 'dimensions and p2 has', len(p2), 
-		      'dimensions; please input points with the same number of dimensions')
+		print('p1 has', len(p1), 'dimensions and p2 has', len(p2),
+		      'dimensions; please input points with the same \
+			  number of dimensions')
 		return -1
 	dist = 0
 	for i in range(len(p1)):
